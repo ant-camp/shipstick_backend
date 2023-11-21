a2812eb277f5d20b7e44f51d7f3b19809c7a0601
Anthony <anttyaadiggg@yahoo.com>
2023-11-21 15:16:03 -0500

    Configure products routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :products
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :products, except: :show do
+    get 'find_by_dimensions', to: 'products#show', on: :collection
+  end
 end
