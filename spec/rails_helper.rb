2c0f4bd02f88040225cb2bfc7e38160640bfbf56
Anthony <anttyaadiggg@yahoo.com>
2023-11-21 15:13:11 -0500

    Fix Rspec configs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,62 +1,50 @@
-ENV['RAILS_ENV'] ||= 'test'
-
-require File.expand_path('../../config/environment', __FILE__)
-abort("The Rails environment is running in production mode!") if Rails.env.production?
-
 require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+# Prevent database truncation if the environment is production
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require 'shoulda/matchers'
-require 'simplecov'
-require 'simplecov-console'
-require 'mongoid-rspec'
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+module ResponseJSON
+  def response_json
+    JSON.parse(response.body)
+  end
+end
 
 RSpec.configure do |config|
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path                               = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures                 = false
   config.infer_base_class_for_anonymous_controllers = false
   config.order                                      = 'random'
   config.use_active_record                          = false
-  config.expect_with(:rspec) { |c| c.syntax         = [:should, :expect] }
+  config.include ResponseJSON
 
-  config.include Mongoid::Matchers, type: :model
   config.include FactoryBot::Syntax::Methods
-  config.include ControllerSpecHelper
-  config.include RequestSpecHelper
-
-
-  config.infer_spec_type_from_file_location!
-  config.filter_rails_from_backtrace!
-
   config.before :all do
     FactoryBot.reload
   end
 
-  config.before :suite do
-    DatabaseCleaner.clean_with :truncation
+  config.expect_with(:rspec) { |c| c.syntax = %i[should expect] }
+
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :deletion
   end
 
-  config.before :each do
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  config.before :each, type: :feature do
-    DatabaseCleaner.strategy = :truncation, { pre_count: true }
-  end
-
-  config.before :each do
-    DatabaseCleaner.start
-  end
-
-  config.after :each do
+  config.before(:each) do
     DatabaseCleaner.clean
   end
-end
 
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
   end
 end
